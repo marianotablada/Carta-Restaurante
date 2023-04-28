1377fd896c24075c97714985b2b9d728ad3eab07
Marian Tablada <mariantablada@Air-de-Marian.home>
2023-04-28 12:07:16 +0200

    Versión Lanzamiento 28 04 23

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const navigation = document.getElementById("nav");
 const menu = document.getElementById("menu");
+const toastContainer = document.getElementById("toast_container");
+const closedBt = document.getElementById("closed");
+
+
+if (!localStorage.getItem("displayToast")){
+setTimeout (() => {
+  toastContainer.classList.add("activeNew");
+},1500);
+}
+
+const stopDisplayinToast = () => {
+  localStorage.setItem("displayToast", false);
+  toastContainer.classList.remove("activeNew");
+}
+
+closedBt.addEventListener("click", () => {
+  stopDisplayinToast();
+  localStorage.clear();
+});
 
 menu.addEventListener("click", () => {
     navigation.style.setProperty("--childenNumber", navigation.children.length);
@@ -7,9 +26,9 @@ menu.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
 
-
 //Captura y/o registro del boton
 let mybutton = document.getElementById("myBtn");
+
 
 // When the user scrolls down 80px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -27,7 +46,4 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   }
-
-
-  
 
